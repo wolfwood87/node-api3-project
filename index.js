1530049490fa0d57176ede9d2a5e7edec63e4396
Derek <legato_5@yahoo.com>
2020-01-21 20:16:16 -0800

    added basic setup for server.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+
+const server = require('./server.js');
+
+server.listen(5000, () => {
+    console.log("*** listening on port 5000")
+})
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter.js')
 const server = express();
+
+server.use(express.json());
+server.use('/api/users/', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-i
+
 function logger(req, res, next) {}
 
 module.exports = server;
