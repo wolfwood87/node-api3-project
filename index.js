4f88f72a7eec1ba1e360b1599079be6ffc837c60
Derek <legato_5@yahoo.com>
2020-01-23 15:57:31 -0800

    changed server to not hardcoded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 
 const server = require('./server.js');
-
-server.listen(5000, () => {
-    console.log("*** listening on port 5000")
+const port = process.env.PORT | 5000;
+server.listen(port, () => {
+    console.log(`*** listening on port ${port}`)
 })
