6542171874d84fc5250cf6166d784113fbefb168
Derek <legato_5@yahoo.com>
2020-01-23 15:46:20 -0800

    all endpoints and middleware now working.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = require('./users/userRouter.js')
+const postRouter = require('./posts/postRouter.js')
 const server = express();
 
 server.use(express.json());
 server.use('/api/users/', userRouter);
+server.use('/api/users/:id/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -11,6 +13,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req);
+  next();
+}
 
 module.exports = server;
+
