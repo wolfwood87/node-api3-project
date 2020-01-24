1997533a643d55a4e082cbeb67d82c86cc13a3a6
Derek <legato_5@yahoo.com>
2020-01-23 16:13:57 -0800

    think I found the error. or operator wasn't typed correctly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const server = require('./server.js');
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`*** listening on port ${port}`)
 })
